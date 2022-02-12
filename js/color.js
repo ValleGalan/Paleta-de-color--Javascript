5c082e9131d1d38f7121dc0cbbd048a2ed70f8c0
Vallegalan <vallegalan810@gmail.com>
2022-02-11 23:04:14 -0300

    subir html de  noticia

--- js/color.js
+++ js/color.js
@@ -1,6 +1,7 @@
 //re recomienda acceder por ID ya que es unico
 const color= document.querySelector("#color");
 const salida =document.querySelector("#salida");
+const salidaTexto=document.querySelector("#salidaTexto");
 
 color.addEventListener("input",()=>{
     const seleccionValor= color.value;//valor del color
@@ -8,4 +9,9 @@ color.addEventListener("input",()=>{
     salida.innerHTML =seleccionValor; //para reemplzar el html innerHTML 
     salida.style.background=seleccionValor; 
     salida.style.color="#fff"; 
+
+    salidaTexto=seleccionValor; //CORREGIR 
+    console.log(seleccionValor);
+    
 })
+ 
